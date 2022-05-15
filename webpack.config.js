b9daa8f0046a2d31c57a3962b590475670568dd5
Johann Barnard <johann.b@telus.net>
2022-05-15 17:42:10 -0600

    Update build tools

--- webpack.config.js
+++ webpack.config.js
@@ -1,51 +1,59 @@
-const path = require('path');
-const autoprefixer = require('autoprefixer');
+const path = require("path");
+const autoprefixer = require("autoprefixer");
 
-const mode = process.env.NODE_ENV == 'production' ? 'production' : 'development';
+const mode =
+  process.env.NODE_ENV == "production" ? "production" : "development";
 
-module.exports = [{
-  mode,
-  entry: ['./webpack/webpack-styles.scss', './webpack/app.js'],
-  output: {
-    path: path.resolve(__dirname, 'assets/generated/'),
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.scss$/,
-        use: [
-          {
-            loader: 'file-loader',
-            options: {
-              name: 'bundle.css',
-            },
-          },
-          { loader: 'extract-loader' },
-          { loader: 'css-loader' },
-          {
-            loader: 'postcss-loader',
-            options: {
-              plugins: () => [autoprefixer()],
-            },
-          },
-          {
-            loader: 'sass-loader',
-            options: {
-              sassOptions: {
-                includePaths: ['./node_modules'],
+module.exports = [
+  {
+    mode,
+    entry: ["./webpack/webpack-styles.scss", "./webpack/app.js"],
+    output: {
+      path: path.resolve(__dirname, "assets/generated/"),
+      filename: "bundle.js",
+    },
+    module: {
+      rules: [
+        {
+          test: /\.scss$/,
+          use: [
+            {
+              loader: "file-loader",
+              options: {
+                name: "bundle.css",
               },
             },
-          },
-        ],
-      },
-      {
-        test: /\.js$/,
-        loader: 'babel-loader',
-        query: {
-          presets: ['@babel/preset-env'],
+            { loader: "extract-loader" },
+            { loader: "css-loader" },
+            {
+              loader: "postcss-loader",
+              options: {
+                postcssOptions: {
+                  plugins: () => [autoprefixer()],
+                },
+              },
+            },
+            {
+              loader: "sass-loader",
+              options: {
+                sassOptions: {
+                  includePaths: ["./node_modules"],
+                },
+              },
+            },
+          ],
         },
-      },
-    ],
+        {
+          test: /\.m?js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: "babel-loader",
+            options: {
+              presets: ["@babel/preset-env"],
+            },
+          },
+        },
+      ],
+    },
   },
-}];
+];
